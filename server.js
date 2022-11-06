1481e8b7b795f67dd3a7cfdd94165ae0b4f19a0f
Ermiyas  Hailemichael <yemeerma11@gmail.com>
2022-11-06 13:53:16 -0800

    create Schema

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 
 require('dotenv').config()
 
 const PORT = process.env.PORT
+
+mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+// DATABASE connection
+const db = mongoose.connection
+db.on('error',(err) => console.log(err.message))
+db.on('connected',()=> console.log('mongo connected'))
+db.on('disconnected',()=>console.log('mongo disconnected'))
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"))
+
+// I        N       D       U       C       E       S
+// INDEX    NEW     DELETE  UPDATE  CREATE  EDIT    SHOW
+
+// CREATE ROUTE
+app.post("/recipes",(req, res)=>{
+    // Recipes.create(req.body,(error, CreatedRecipes)=>{
+    //     res.send(req.body)
+    // })
+    // res.send(req.body)
+    // res.send('sending...')
+})
 
 app.listen(PORT,()=>console.log(`Server is live on port :${PORT}`))
