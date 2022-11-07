1809ed4155743c4c1d26adff2f57145a1c934f67
Ermiyas  Hailemichael <yemeerma11@gmail.com>
2022-11-07 04:26:49 -0800

    mulitple update and edit form works

--- server.js
+++ server.js
@@ -1,69 +1,77 @@
 // DEPENDENCIES
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const methodOverride = require('method-override')
-const Recipes = require('./models/recipes')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const Recipes = require("./models/recipes");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-mongoose.connect(process.env.DATABASE_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // DATABASE connection
-const db = mongoose.connection
-db.on('error',(err) => console.log(err.message))
-db.on('connected',()=> console.log('mongo connected'))
-db.on('disconnected',()=>console.log('mongo disconnected'))
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err.message));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
 
 // MIDDLEWARE
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"))
+app.use(methodOverride("_method"));
 
 // I        N       D       U       C       E       S
 // INDEX    NEW     DELETE  UPDATE  CREATE  EDIT    SHOW
 
 // INDEX
-app.get("/recipes",(req, res)=>{
-    Recipes.find({},(error, allrecipes)=>{
-        res.render("index.ejs",{
-        recipes: allrecipes,
+app.get("/recipes", (req, res) => {
+  Recipes.find({}, (error, allrecipes) => {
+    res.render("index.ejs", {
+      recipes: allrecipes,
+    });
+  });
+});
+// NEW
+app.get("/recipes/new", (req, res) => {
+  res.render("new.ejs");
+  // res.send('New works')
+});
+
+// DELETE
+app.delete("/recipes/:id", (req, res) => {
+  // res.send("Delete works")
+  Recipes.findByIdAndRemove(req.params.id, (error, deletedrecipes) => {
+    console.log(deletedrecipes);
+    res.redirect("/recipes");
+  });
+});
+// CREATE ROUTE
+app.post("/recipes", (req, res) => {
+  Recipes.create(req.body, (error, CreatedRecipes) => {
+    res.redirect("/recipes");
+  });
+});
+
+// Edit
+app.get("/recipes/:id/edit",(req, res)=>{
+    Recipes.findById(req.params.id,(error, foundRecipes)=>{
+        // res.send(foundRecipes)
+        res.render('edit.ejs',{
+            recipes: foundRecipes
         })
     })
 })
-// NEW
-app.get('/recipes/new', (req, res)=>{
-    res.render('new.ejs')
-    // res.send('New works')
-})
-
-// DELETE
-app.delete("/recipes/:id",(req, res)=>{
-    // res.send("Delete works")
-    Recipes.findByIdAndRemove(req.params.id, (error,deletedrecipes)=>{
-        console.log(deletedrecipes)
-        res.redirect('/recipes')
-    })
-
-})
-// CREATE ROUTE
-app.post("/recipes",(req, res)=>{
-    Recipes.create(req.body,(error, CreatedRecipes)=>{
-    res.redirect('/recipes');
-    })
-})
-
 // SHOW
 app.get("/recipes/:id", (req, res) => {
-    Recipes.findById(req.params.id, (error, foundRecipes) => {
-      // res.send(foundRecipes);
-      res.render("show.ejs", {recipes: foundRecipes})
-    });
-    // res.send("Works, SHOW")
+  Recipes.findById(req.params.id, (error, foundRecipes) => {
+    //   res.send(foundRecipes);
+    res.render("show.ejs", { recipes: foundRecipes });
   });
+  // res.send("Works, SHOW")
+});
 
-app.listen(PORT,()=>console.log(`Server is live on port :${PORT}`))
+app.listen(PORT, () => console.log(`Server is live on port :${PORT}`));
